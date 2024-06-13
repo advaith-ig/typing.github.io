e8312e3fd857d27f287721ae245762ae5b346351
advaith-ig <122174668+advaith-ig@users.noreply.github.com>
2024-06-13 21:38:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,100 +1,169 @@
-const typingText = document.querySelector(".typing-text p"),
-    inpField = document.querySelector(".wrapper .input-field"),
-    tryAgainBtn = document.querySelector(".tryagain"),
-    timeTag = document.querySelector(".time"),
-    wpmTag = document.querySelector(".wpm");
+@import url('https://fonts.googleapis.com/css2?family=Courier+Prime:wght@400;500;600;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@400;500;600;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Alegreya+Sans+SC:wght@400;500;600;700&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Cute+Font&display=swap');
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: 'Montserrat';
+  font-size: 16px;
+}
+body{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-top: 30px;
+  min-height: 80vh;
+  background: #07011b; /*07011b*/
+  
+}
+.credits{
+  color: #9afaff;
+  position: absolute;
+  bottom: 20px;
+  left: 50%;
+  transform: translateX(-50%);
+  color: #43d0f0;
+  font-family: 'Cute Font';
+  font-size: 18px;
+  font-weight: 500;
+  letter-spacing: 1.5px;
+}
+.tracks {
+  display: flex;
+  gap: 10px;
+  color: #cceefe;
+  text-shadow: 
+    0 0 1px #9afaff, 
+    0 0 1px #00c8ff;
+  margin-bottom: 15px;
+  width: max-content;
+  font-size: 18px;  
+  font-family: 'Montserrat';
+  font-weight: 500;
+  letter-spacing: 1.5px;
+  background-color: #05293a;
+  padding: 5px;
+  padding-left: 15px; 
+  padding-right: 15px;
+  border-radius: 6px;
+}
+.tracks .timetrack{
+  margin-left: 20px;
+}
+.wrapper{
+  width: 900px;
+  margin-top: 50px;
+  padding: 10px;  
+  background: #07011b;
+  border-radius: 20px;
+  box-shadow: 0 10px 15px rgba(0,0,0,0.05);
+}
+.wrapper .input-field{
+  opacity: 0;
+  
+}
+.wrapper .content-box{
+  padding: 40px 55px 40px;
+  border-radius: 15px;
+  font-size: 18px;
+  line-height: 30px;
+  border: 3px solid #c8f8fb;
+  
+  box-shadow: 0 0 .5rem #fff,
+  0 0 .01rem #fff,
+  0 0 .01rem #13fe52,
+  0 0 0.01rem #99d6d3,
+  0 0 0.01rem #99d6d3,
+  inset 0 0 0.5rem #99d6d3;
+}
+.content-box .typing-text{
+  color: #326e89;
+  /*#a3008d  #494072 */
+  
+  overflow:hidden;
+  max-height: 256px;
+}
+.typing-text p{
+  font-size: 28px;
+  text-align: left;
+  letter-spacing: 2px;
+}
+.typing-text p span{
+  position: relative;
+}
+.typing-text p span.correct{
+  color: #9dffe3;
+  text-shadow: 
+  0 0 10px #bafaff, 
+  0 0 15px #108ba3;
+}
+.typing-text p span.incorrect{
+  color: #ff0008;
+  /*background-color: #ff000025;*/
+  text-shadow: 
+  0 0 1px #ea469b, 
+  0 0 1px #ff008c;
+  border-radius: 4px;
+}
+.typing-text p span.incorrect-space {
+  background-color: #ff000032;
+  text-shadow: 0 0 1px #ea469b, 0 0 1px #ff008c;
+  border-radius: 4px;
+}
+.typing-text p span.active {
+  border-left: .15em solid #cbfff7;
+}
 
-let timer,
-    maxTime = 60,
-    elapsed = 0,
-    timeLeft = maxTime,
-    charIndex = 0,
-    mistakes = 0,
-    isTyping = 0;
+@keyframes cursor-move {
+  from {
+    transform: scaleX(0);
+  }
+  to {
+    transform: scaleX(1);
+  }
+}
 
-function loadParagraph() {
-    typingText.innerHTML = "";
-    for (let i = 0; i < 50; i++) {
-        const ranIndex = Math.floor(Math.random() * words.length);
-        words[ranIndex].split("").forEach(char => {
-            let span = `<span>${char}</span>`; 
-            typingText.innerHTML += span;
-        });
-        if (i !== 49) {
-            typingText.innerHTML += "<span> </span>";
-        }
-    }
-    typingText.querySelectorAll("span")[0].classList.add("active");
-    document.addEventListener("keydown", handleKeydown);
-    typingText.addEventListener("click", () => inpField.focus());
+.typing-text p span.active::before{
+  position: relative;
+  content: "";
+  height: 2px;
+  width: 100%;
+  bottom: 0;
+  left: 0;
+  opacity: 0;
+  border-radius: px;
+  background: #17A2B8;
+  animation: ease-in-out;
+  transition: 1s ease-in-out;
+}
+
+.content-box .tryagain:active{
+  transform: scale(0.97);
 }
 
 
-function handleKeydown(e) {
-    inpField.focus();
-    if (e.ctrlKey && e.key === "Backspace") {
-        e.preventDefault();
-    }
+.logo {
+  position: absolute;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 999;
 }
 
-
-function typingF() {
-    let characters = typingText.querySelectorAll("span");
-    let typedChar = inpField.value.charAt(charIndex);
-    if (charIndex < characters.length && timeLeft > 0) {
-        if (!isTyping) {
-            timer = setInterval(timeF, 1000);
-            isTyping = true;    
-        }
-        if (typedChar === "") {
-            if (charIndex > 0) {
-                charIndex--;
-                characters[charIndex].classList.remove("correct", "incorrect");
-            }
-        } else {
-            if (characters[charIndex].innerText === typedChar) {
-                characters[charIndex].classList.add("correct");
-            } else {
-                characters[charIndex].classList.add("incorrect");
-                mistakes++;
-            }
-            charIndex++;
-        }
-        characters.forEach(span => span.classList.remove("active"));
-        if (charIndex < characters.length) {
-            characters[charIndex].classList.add("active");
-        }
-
-        let wpm = Math.round(((charIndex - mistakes) / 5) / (maxTime - timeLeft) * 60);
-        wpm = wpm < 0 || !wpm || wpm === Infinity ? 0 : wpm;
-
-        wpmTag.innerText = wpm;
-    } else {
-        clearInterval(timer);
-        inpField.value = null;
-    }
+.tryagain {
+  position: relative;
+  float: right;
+  outline: none;
+  border: none;
+  color: #fff;
+  margin-top: 10px;
+  padding: 8px 15px;
+  font-size: 16px;
+  cursor: pointer;
+  border-radius: 5px;
+  background: #17A2B8;
+  transition: transform 0.3s ease;
 }
-function timeF() {
-    if (timeLeft > 0) {
-        timeLeft--;
-        timeTag.innerText = timeLeft;
-        let wpm = Math.round(((charIndex - mistakes) / 5) / (maxTime - timeLeft) * 60);
-        wpmTag.innerText = wpm;
-    } else {
-        clearInterval(timer);
-    }
-}
-
-function resetGame() {
-    loadParagraph();
-    clearInterval(timer);
-    timeLeft = maxTime;
-    charIndex = mistakes = isTyping = 0;
-    inpField.value = "";
-    timeTag.innerText = timeLeft;
-    wpmTag.innerText = 0;
-}
-
-loadParagraph();
-inpField.addEventListener("input", typingF);
-tryAgainBtn.addEventListener("click", resetGame);
